8571a0905a65012bd7e3cbb79051c1ebe8d58282
SherdinVoid <SherdinVoid@gmail.com>
2025-03-23 00:00:01 +0200

    Added first lection

---
+++ Lesson_1/Task_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    cout << "Are you sure?" << endl;
+    cout << "After all, I am SHERDIN VOID" << endl;
+    return 0;
+}
